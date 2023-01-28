d16a509e80cb47d2dfdc4c492bfc1a4b8d58b876
Lautaro Garibaldi <lautaro.garibaldi@gmail.com>
2023-01-28 06:30:57 -0300

    ItemListConteiner agregado

--- logic-work/src/App.jsx
+++ logic-work/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import NavBar from './componentes/NavBar/NavBar'
+import ItemListConteiner from './componentes/ItemListConteiner/ItemListConteiner'
 
 function App() {
 
@@ -9,7 +10,7 @@ function App() {
     <div className="App">
       <h1>Logic-Work</h1>
       <NavBar />
-     <h3>Bienvenido a Logic Work! los mejores precios del mercado en componentes de pc!</h3>
+     <ItemListConteiner name = "Lautaro"/>
     </div>
   )
 }
---
+++ logic-work/src/componentes/ItemListConteiner/ItemListConteiner.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ItemListConteiner = (props) => {
+  return (
+    <div><h3>Hola, {props.name}, bienvenido a Logic Work! Los mejores compentes de PC al menor precio!. </h3></div>
+  )
+}
+
+export default ItemListConteiner
